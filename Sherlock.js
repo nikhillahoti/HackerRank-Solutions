5ce456fc895be89d88f7c5afac5c8dab01c3492a
Nikhil Lahoti <lahotinikhil92@gmail.com>
2018-06-10 19:14:13 -0700

    Sherlock update

--- Sherlock.js
+++ Sherlock.js
@@ -1,10 +1,34 @@
-
-
 function isValid(s){
-
+    let dic = {};
     for(let i = 0 ; i < s.length ; i++){
-        console.log(s[i]);
+        if(s[i] in dic) dic[s[i]] += 1;
+        else dic[s[i]] = 1;
     }
+    
+    let nameDic = {};
+    for(let key in dic){
+        if(dic[key] in nameDic){
+            nameDic[dic[key]] = nameDic[dic[key]] + 1;
+        }
+        else{
+            if(Object.keys(nameDic).length > 2){
+                return "NO";
+            }
+            nameDic[dic[key]] = 1;
+        } 
+    }
+    
+    if(Object.keys(nameDic).length > 1){
+        let firstKey = Object.keys(nameDic)[0];
+        let secondKey = Object.keys(nameDic)[1];
+
+        if((nameDic[firstKey] == 1 && ( secondKey == firstKey - 1)) || 
+            (nameDic[secondKey] == 1 && ( firstKey == secondKey - 1)) || 
+            (nameDic[firstKey] == 1 && firstKey == 1) ||
+            (nameDic[secondKey] == 1 && secondKey == 1)  ) return "YES";
+        else return "NO";
+    }
+    return "YES";
 }
 
-isValid("Nikhil");
+console.log(isValid("aabbccddeefghi"));
